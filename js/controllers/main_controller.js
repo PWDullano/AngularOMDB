88bff85a71b05604774d027cddcd70bcb956728c
Patrick Dullano <PWDullano@gmail.com>
2016-02-24 09:19:32 -0700

    More information being displayed in show page

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -3,11 +3,9 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http){
 $scope.submitPost = function(){
     $http.get("http://www.omdbapi.com/?s=" + $scope.input).then(function(payload){
       $scope.movies = [];
-      console.log(payload);
       payload.data.Search.forEach(function(movie){
       $scope.movies.push(movie)
     })
-    console.log($scope.movies);
   })
  }
 }])
